0b20c667cb88a9d4dda4bf29dafe8641f61f4b5a
Jason Bui <jasonbui@169-231-74-100.wireless.ucsb.edu>
2025-02-22 15:25:40 -0800

    fix

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -9,5 +9,6 @@ class Solution:
                 return mid 
             elif mid * mid < x:
                 left = mid + 1 
-            else: right = mid - 1
+            else: 
+                right = mid - 1
         return right
