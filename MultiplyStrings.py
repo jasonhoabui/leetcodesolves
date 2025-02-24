a2326168db95fc8816599c1385c85c89adf9b055
Jason Bui <jasonbui@169-231-74-100.wireless.ucsb.edu>
2025-02-23 18:47:34 -0800

    multiplystrings but cheated

---
+++ MultiplyStrings.py
@@ -0,0 +1,5 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        n = int(num1)
+        m = int(num2)
+        return str(n * m) #return to this when you can do it without using int() LMFAO
