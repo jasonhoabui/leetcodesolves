adf209e06dcf4092d68597b209e14ddd20b4b75c
Jason Bui <jasonbui@169-231-74-100.wireless.ucsb.edu>
2025-02-23 21:35:39 -0800

    some solves

--- ValidPalindrome.py
+++ ValidPalindrome.py
@@ -1,8 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = [i.lower() for i in s if i.isalnum()]
-        if len(s) == 0 or len(s) == 1:
-            return True
-        if s[0] == s[-1]:
-            return self.isPalindrome(s[1:-1])
-        return False
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1 
+
+        return True
+        #pointer method
