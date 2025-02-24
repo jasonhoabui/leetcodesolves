cde5944edf64113a03ecf5a5d26fc3af1b567d3a
Jason Bui <jasonbui@169-231-74-100.wireless.ucsb.edu>
2025-02-23 18:33:05 -0800

    palindromnumber

---
+++ PalindromeNumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        if len(x) == 0 or len(x) == 1:
+            return True
+        if x[0] == x[-1]:
+            return self.isPalindrome(x[1:-1])
+        return False
